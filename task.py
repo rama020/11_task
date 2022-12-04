86e732b8b504e65dde26862002859b128f206ce7
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-04 16:09:19 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,32 @@
+'''
+DATE:04TH NOV 2022
+DAY: SUNDAY
+TOPIC: TASK
+AUTHOR: RAMA BHARGAVI
+'''
+name=input('enter name:')
+id=int(input('enter id:'))
+salary=float(input('enter salary:'))
+print('name is: {name},id is: {id} salary is: {salary}')
+a,b,c=5,4,3
+sum=a+b+c
+print(sum)2
+avg=sum/3
+print(avg)
+#area of circle
+r=int(input('enter the radius:'))
+Py=22/7
+area=Py*r**2
+print('the area of circle is %.2f' %area)
+#get_char_disply
+c=input('enter the char')
+print('the char is: '+c[3])
+#joining_strings
+a='bhargavi'
+b='12'
+print(a+b)
+#cude_numbers
+a=4
+print(a**3)
+for i in range(1,5):
+    print(i**3,end=" ")
