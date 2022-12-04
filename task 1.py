1c97fccbb73af891d95488e67db53b37cce09419
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-04 19:50:06 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,19 @@
+'''
+DATE:04TH NOV 2022
+DAY: SUNDAY
+TOPIC: TASK
+AUTHOR: RAMA BHARGAVI
+'''
+#even_odd
+#for i in range(1,20,2):
+ #   print(i,end=' ,')
+#for i in range(2,20,2):
+ #   print(i,end=' ,')
+#even_odd_zero
+#n=int(input('enter a numbetr'))
+#if(n%2==0):
+ #   print(f'{n} is even number')
+#else:
+ #   print(f'{n} is odd number')
+ 
+ 
